147b2740ca45643c074c1ac05996a89f8fef2018
irehankhan <irehankhan@yahoo.com>
2023-12-08 16:38:54 +0500

    Modification

--- client.py
+++ client.py
@@ -75,4 +75,4 @@ def get_word_count(text):
     return len(text.split())
 
 if __name__ == "__main__":
-    app.run(debug=True, host='0.0.0.0', port=5010)
+    app.run(debug=True, host='0.0.0.0', port=80)
